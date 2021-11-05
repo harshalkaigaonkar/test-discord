0a12de4896cf5fd5edb1b21df622b3d1a0d9a5b8
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-11-05 19:53:33 +0530

    feat: 3 slash commands

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-require('config.json');
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
+const { token } = require('./config.json');
+require('./deploy-commands.js');
 
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }
+});
+
+// Login to Discord with your client's token
+client.login(token);
